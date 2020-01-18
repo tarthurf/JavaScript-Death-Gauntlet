100f4f70592900f69f910b85a320fdbb5ada64df
Swedish <tarthurf@yahoo.com>
2020-01-17 23:54:11 -0800

    Added function to start a timer; added a function that selects a random number up to a max

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const timerPenalty = 1
 
 const selectedQuestions = [];
 let currentQuestion = {};
+
+function randomUpToMax(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function startTimer(timeLimit) {
+    timerCounter = timeLimit;
+    const timer = setInterval(function(){
+      $("#timer").text(timerCounter);
+      timerCounter--;
+      if (timerCounter <= 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
