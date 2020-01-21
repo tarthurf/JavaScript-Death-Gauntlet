19069190f5096f26474b47da1f689c8a2ad18cdd
Swedish <tarthurf@yahoo.com>
2020-01-20 22:02:24 -0800

    added function that penalizes the user for selecting a wrong answer

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const timerPenalty = 1
 const selectedQuestions = [];
 let currentQuestion = {};
 
-const buttonNumber = (i + 1);
-const buttonNumberToString = buttonNumber.toString();
-
 // this functions grabs a random question object from myQuestions.js
 function selectQuestion() {
   currentQuestion = myQuestions.splice(randomUpToMax(myQuestions.length), 1).pop();
@@ -25,22 +22,21 @@ function displayQuestion(element) {
 	$(element).text(currentQuestion.question);
 }
 
+// this function adds buttons with specific answers on them
 function displayAnswerButtons(element, x) {
   for (let i = 0; i < x; i++) {
-    // const buttonNumber = (i + 1);
-    // const buttonNumberToString = buttonNumber.toString();
+    const buttonNumber = (i + 1);
+    const buttonNumberToString = buttonNumber.toString();
     $(element + buttonNumberToString).text(currentQuestion.answers[buttonNumber]);
-
-    console.log($(element + i));
   }
 }
 
-// TODO: Create function that checks the correct answer and adds values to the DOM elements
+// this function sets a boolean to right and wrong dom elements
 function setAnswerValues(element, x) {
   const objectKeyArray = Object.keys(currentQuestion.answers)
   for (let i = 0; i < x; i++) {
-    // const buttonNumber = (i + 1);
-    // const buttonNumberToString = buttonNumber.toString();
+    const buttonNumber = (i + 1);
+    const buttonNumberToString = buttonNumber.toString();
     console.log(objectKeyArray)
     if (objectKeyArray[i] === currentQuestion.correctAnswer) {
       $(element + buttonNumberToString).attr("value", true)
@@ -51,10 +47,13 @@ function setAnswerValues(element, x) {
   }
 }
 
-// TODO: functionize timer penalty
-// function timerPenalty() {
-
-// }
+// This function penalizes the user for guessing a wrong answer
+function timerPenaltyOnButton(element) {
+  if (timerCounter > 0 && $(element).val("false")) {
+    timerCounter -= timerPenalty;
+    updatePageTimer();
+  }
+}
 
 // this function will start a timer
 function startTimer(timeLimit) {
@@ -71,29 +70,23 @@ function startTimer(timeLimit) {
 // this function will add any number of buttons to an element in HTML
 function addButtons(parentEl, x) {
   for (let i = 0; i < x; i++) {
-    // const buttonNumber = (i + 1);
-    // const buttonNumberToString = buttonNumber.toString();
+    const buttonNumber = (i + 1);
+    const buttonNumberToString = buttonNumber.toString();
     $(parentEl).append("<button></button>")
     $(parentEl + ":nth-child(" + buttonNumberToString + ")").attr({id:"button" + buttonNumber, type:"button"});
-    // console.log(`${"button" + i.toString()}`);
   }
 }
 
-$("#button2").on("click", function() { // this is only for testing purposes
-  displayQuestion();
-})
 
-$("#start-timer").on("click", () => startTimer(5)); // this is only for testing purposes
+
+$("#start-timer").on("click", () => startTimer(30)); // this is only for testing purposes
 
 selectQuestion(); //This is just for testing purposes
 console.log(currentQuestion.correctAnswer); //This is just for testing purposes
 console.log(Object.keys(currentQuestion)); //This is just for testing purposes
 
-$("#button1").text(currentQuestion.answers.a).on("click", function() { // This is only for testing purposes
-  if (timerCounter > 0) {
-    timerCounter -= timerPenalty;
-    updatePageTimer();
-  }
+$("button").on("click", function() { // This is only for testing purposes
+  timerPenaltyOnButton("button")
 });
 
 $("#button3").on("click", () => addButtons(".answer-buttons", 5)); // this is only for testing purposes
@@ -102,4 +95,3 @@ $("#button4").on("click", () => displayAnswerButtons("#button", 5)); // this is 
 
 $("#button5").on("click", () => setAnswerValues("#button", 5)); // this is only for testing purposes
 
-// functionize wrong answers
