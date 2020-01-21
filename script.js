3c4c14fb5b2db920a27b58a8a555e63a2ebb542a
Swedish <tarthurf@yahoo.com>
2020-01-20 22:46:50 -0800

    Fixed timer penalty function, created anonymous function tied to button element instead

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ const timerPenalty = 1
 const selectedQuestions = [];
 let currentQuestion = {};
 
-// this functions grabs a random question object from myQuestions.js
-function selectQuestion() {
-  currentQuestion = myQuestions.splice(randomUpToMax(myQuestions.length), 1).pop();
-}
 
 function randomUpToMax(max) {
     return Math.floor(Math.random() * max);
 }
 
+// This function updates a the timer on the page
 function updatePageTimer() {
     $("#timer").text(timerCounter);
+}
+
+// this functions grabs a random question object from myQuestions.js
+function selectQuestion() {
+  currentQuestion = myQuestions.splice(randomUpToMax(myQuestions.length), 1).pop();
 }
 
 // this function displays a question from questions.js
@@ -31,30 +33,6 @@ function displayAnswerButtons(element, x) {
   }
 }
 
-// this function sets a boolean to right and wrong dom elements
-function setAnswerValues(element, x) {
-  const objectKeyArray = Object.keys(currentQuestion.answers)
-  for (let i = 0; i < x; i++) {
-    const buttonNumber = (i + 1);
-    const buttonNumberToString = buttonNumber.toString();
-    console.log(objectKeyArray)
-    if (objectKeyArray[i] === currentQuestion.correctAnswer) {
-      $(element + buttonNumberToString).attr("value", true)
-    }
-    else {
-      $(element + buttonNumberToString).attr("value", false)
-    }
-  }
-}
-
-// This function penalizes the user for guessing a wrong answer
-function timerPenaltyOnButton(element) {
-  if (timerCounter > 0 && $(element).val("false")) {
-    timerCounter -= timerPenalty;
-    updatePageTimer();
-  }
-}
-
 // this function will start a timer
 function startTimer(timeLimit) {
   timerCounter = timeLimit;
@@ -67,29 +45,40 @@ function startTimer(timeLimit) {
   }, 1000);
 }
 
-// this function will add any number of buttons to an element in HTML
-function addButtons(parentEl, x) {
+
+// this function sets a boolean to right and wrong dom elements
+function setAnswerValues(element, x) {
+  const objectKeyArray = Object.keys(currentQuestion.answers)
   for (let i = 0; i < x; i++) {
     const buttonNumber = (i + 1);
     const buttonNumberToString = buttonNumber.toString();
-    $(parentEl).append("<button></button>")
-    $(parentEl + ":nth-child(" + buttonNumberToString + ")").attr({id:"button" + buttonNumber, type:"button"});
+    console.log(objectKeyArray[i]);
+    console.log(currentQuestion.correctAnswer);
+    if (objectKeyArray[i] === currentQuestion.correctAnswer) {
+      $(element + buttonNumberToString).attr("value", true)
+    }
+    else {
+      $(element + buttonNumberToString).attr("value", false)
+    }
   }
 }
 
-
+// This function penalizes the user for guessing a wrong answer
+$("button").on("click", function() {
+  console.log(this);
+  let buttonValue = $(this).attr("value");
+  console.log(`Button value is: ${buttonValue}`);
+  if (timerCounter > 0 && buttonValue === "false") {
+    timerCounter -= timerPenalty;
+    updatePageTimer();
+  } 
+});
 
 $("#start-timer").on("click", () => startTimer(30)); // this is only for testing purposes
 
 selectQuestion(); //This is just for testing purposes
 console.log(currentQuestion.correctAnswer); //This is just for testing purposes
 console.log(Object.keys(currentQuestion)); //This is just for testing purposes
-
-$("button").on("click", function() { // This is only for testing purposes
-  timerPenaltyOnButton("button")
-});
-
-$("#button3").on("click", () => addButtons(".answer-buttons", 5)); // this is only for testing purposes
 
 $("#button4").on("click", () => displayAnswerButtons("#button", 5)); // this is only for testing purposes
 
