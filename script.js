da7ceeb55035e778828d3d7dfd1c81e6597b541a
Swedish <tarthurf@yahoo.com>
2020-01-22 21:34:20 -0800

    Started coding the first input once the user hits the start button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let timerCounter = 0
-const timerPenalty = 1
+let timerCounter;
+const timerPenalty = 1;
 
 let selectedQuestions = [];
 let currentQuestion = {};
-
 
 function randomUpToMax(max) {
   return Math.floor(Math.random() * max);
@@ -12,44 +11,6 @@ function randomUpToMax(max) {
 // This function updates a the timer on the page
 function updatePageTimer() {
   $("#timer").text(timerCounter);
-}
-
-// this functions grabs a random question object from myQuestions.js
-function selectQuestion() {
-  currentQuestion = myQuestions.splice(randomUpToMax(myQuestions.length), 1).pop();
-}
-
-// function that moves current question to selected question array
-function changeToNextQuestion() {
-  selectedQuestions.push(currentQuestion);
-  currentQuestion = {}
-}
-
-// this function displays a question from questions.js
-function displayQuestion(element) {
-	$(element).text(currentQuestion.question);
-}
-
-// this function will add any number of buttons to an element in HTML
-function addButtons(parentEl, x) {
-  for (let i = 0; i < x; i++) {
-    const newButton = $('<button>').attr({id:"button" + (i+1)});
-    $(parentEl).append(newButton);
-  }
-}
-// addButtons("#answer-buttons", 5);
-
-function deleteElementsFromParent(parentEl) {
-  $(parentEl).empty();
-}
-
-// this function adds buttons with specific answers on them
-function displayAnswerButtons(element, x) {
-  for (let i = 0; i < x; i++) {
-    const buttonNumber = (i + 1);
-    const buttonNumberToString = buttonNumber.toString();
-    $(element + buttonNumberToString).text(currentQuestion.answers[buttonNumber]);
-  }
 }
 
 // this function will start a timer
@@ -64,16 +25,41 @@ function startTimer(timeLimit) {
   }, 1000);
 }
 
+// this functions grabs a random question object from myQuestions.js
+function selectQuestion() {
+  currentQuestion = myQuestions.splice(randomUpToMax(myQuestions.length), 1).pop();
+}
+
+// this function displays a question from questions.js
+function displayQuestion(element) {
+  $(element).text(currentQuestion.question);
+}
+
+// this function will add any number of buttons to an element in HTML
+function addButtons(parentEl, x) {
+  for (let i = 0; i < x; i++) {
+    const newButton = $('<button>').attr({id:"button" + (i+1)});
+    $(parentEl).append(newButton);
+  }
+}
+
+// this function adds buttons with specific answers on them
+function displayAnswerButtons(element, x) {
+  for (let i = 0; i < x; i++) {
+    const buttonNumber = (i + 1);
+    const buttonNumberToString = buttonNumber.toString();
+    $(element + buttonNumberToString).text(currentQuestion.answers[buttonNumber]);
+  }
+}
+
 // this function sets a boolean to right and wrong dom elements
 function setAnswerValues(element, x) {
   const objectKeyArray = Object.keys(currentQuestion.answers)
   for (let i = 0; i < x; i++) {
     const buttonNumber = (i + 1);
     const buttonNumberToString = buttonNumber.toString();
-    // console.log(objectKeyArray[i]);
-    // console.log(currentQuestion.correctAnswer);
     if (objectKeyArray[i] === currentQuestion.correctAnswer) {
-      $(element + buttonNumberToString).attr("value", true)
+      $(element + buttonNumberToString).attr({value: true, onclick: startNextQuestion()})
     }
     else {
       $(element + buttonNumberToString).attr("value", false)
@@ -81,39 +67,57 @@ function setAnswerValues(element, x) {
   }
 }
 
+// TODO: create function that will start a new set of questions to display
+// use setTimeout() = to timerCounter * 1000 while user is choosing a question
+// When user selects the correct question this function runs, starting with clearTimeout()
+// load next question
+// reset setTimeout() to = current timerCounter
+function startNextQuestion() {
+
+}
+
+// function that moves current question to selected question array
+function changeToNextQuestion() {
+  selectedQuestions.push(currentQuestion);
+  currentQuestion = {}
+}
+
+function deleteElementsFromParent(parentEl) {
+  $(parentEl).empty();
+}
+
+
 // Testing Grounds!!!
-$("#start-timer").on("click", () => startTimer(30));
-  
-deleteElementsFromParent("#answer-buttons");
-addButtons("#answer-buttons", 5);
-
-selectQuestion();
-console.log(currentQuestion);
-
-changeToNextQuestion();
-console.log(selectedQuestions);
-console.log(currentQuestion);
-console.log(myQuestions.length)
 
 // Testing Grounds!!!
 
-$("#button4").on("click", () => displayAnswerButtons("#button", 5)); // this is only for testing purposes
 
-$("#button5").on("click", () => setAnswerValues("#button", 5)); // this is only for testing purposes
-
-$(document).ready(function(){
-  // This function penalizes the user for guessing a wrong answer
-  $("button").on("click", function() {
-    console.log(this);
-    let buttonValue = $(this).attr("value");
-    console.log(`Button value is: ${e.target}`);
-    if (timerCounter > 0 && buttonValue === "false") {
-      timerCounter -= timerPenalty;
-      updatePageTimer();
-    } 
-  });
-  
+// TODO: Fix timer penalty on wrong answer click
+$("button").on("click", function() {
+  let buttonValue = $(this).attr("value");
+  if (timerCounter > 0 && buttonValue === "false") {
+    timerCounter -= timerPenalty;
+    updatePageTimer();
+  }
 });
+
+function startGame() {
+
+  $(document).ready(function(){
+    $("#start-game").hide();
+    startTimer(30)
+    if (timerCounter > 0) {
+      selectQuestion();
+      displayQuestion("#question")
+      addButtons("#answer-buttons", 5)
+      displayAnswerButtons("#button", 5)
+      
+      
+      // deleteElementsFromParent("#answer-buttons");
+      // addButtons("#answer-buttons", 5);
+    }   
+  });
+}
 
 // When game starts
 
@@ -133,7 +137,9 @@ $(document).ready(function(){
   // random question added to current question
   // question is displayed in element
   // answer buttons created
-  // answers appended to button
+  // answers appended to button 
   // values added to button
 
 // Repeat until time runs out
+
+// TODO: control start button through js. show button after game.
